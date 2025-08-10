de8eafdb70fdb6d2e45b7bdf2bea0d576535b096
Injamhossan <injamhossanmamun@gmail.com>
2025-08-10 22:02:54 +0600

    Max new

---
+++ max3.js
@@ -0,0 +1,18 @@
+const jim = 57;
+const tim = 46;
+const kim = 93;
+
+if (jim > tim && jim > kim) {
+    console.log('Jim is the ultimate boss');
+}
+else if (tim > jim && tim > kim) {
+    console.log('Tim is the ultimate boss');
+}else {
+    console.log('kim is the ultimate boss');
+}
+
+function maxOfThree (num1, num2, num3) {
+}
+
+const max = Math.max(12, 1, 56, 5, 6, 356, 854, 46, 235);
+console.log('Max isuing Math.Max',max)
