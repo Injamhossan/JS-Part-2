302d6f917a7386d7934927557d830eaf16e0116f
Injamhossan <injamhossanmamun@gmail.com>
2025-08-16 19:37:11 +0600

    Object

---
+++ object.js
@@ -0,0 +1,19 @@
+const student = {
+    name: 'Sakib Khan',
+    id: 121,
+    address: 'movie cinema',
+    isSingle: true,
+    friends: ['Apu', 'Raaz', 'Rahul', 'Rasel'],
+    movies: [{name: 'no-1', year: 2015}, {name: 'no-2', year: 2016}],
+    car: {
+        model: 'Toyota',
+        year: 2015,
+        price: '50000$',
+        manufacturer: {
+            name: 'Toyota',
+            country: 'Japan',
+        }
+    }
+}
+
+console.log(student);
