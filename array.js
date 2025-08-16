b5b90ed90901366484decd6cd253f29e69c6077d
Injamhossan <injamhossanmamun@gmail.com>
2025-08-16 19:56:46 +0600

    array

---
+++ array.js
@@ -0,0 +1,8 @@
+const products = [
+    {name: 'Product 1', price: 10.99, quantity: 5},
+    {name: 'Product 2', price: 9.99, quantity: 10}   
+]
+
+const product = {
+    "Product 1": {price: 10.99, quantity: 5}
+}
